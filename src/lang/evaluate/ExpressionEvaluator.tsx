947359f6f19a77e92520d2ec26488927c2236081
Wesley Luk <wesleyluk@gmail.com>
2021-03-06 14:32:28 -0800

    Fix compile

--- src/lang/evaluate/ExpressionEvaluator.tsx
+++ src/lang/evaluate/ExpressionEvaluator.tsx
@@ -1,5 +1,6 @@
 import { Option } from "fp-ts/lib/Option";
 import { Expr } from "../parse/Expression";
+import { Evaluator } from "./Evaluator";
 import { Result } from "./Result";
 
 export abstract class ExpressionEvaluator {
