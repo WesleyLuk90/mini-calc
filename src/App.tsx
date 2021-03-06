c2d4a36492be5a5e4b6595945f5e2bb5d29f0619
Wesley Luk <wesleyluk@gmail.com>
2021-03-05 17:07:44 -0800

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { Button, Container } from "react-bootstrap";
 import "./App.css";
+import { ExpressionSection } from "./ExpressionSection";
+export function App() {
+    const [sections, setSections] = useState<number[]>([]);
 
-function App() {
-    return <div className="App"></div>;
+    function addSection() {
+        return setSections([...sections, 1]);
+    }
+
+    return (
+        <Container>
+            {sections.map((s, i) => (
+                <ExpressionSection key={i}></ExpressionSection>
+            ))}
+            <Button
+                variant="outline-primary"
+                size="sm"
+                block
+                onClick={addSection}
+            >
+                <FontAwesomeIcon icon={faPlus} />
+            </Button>
+        </Container>
+    );
 }
-
-export default App;
---
+++ src/ExpressionSection.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./ExpressionSection.css";
+
+export function ExpressionSection() {
+    return <div className="expression-section">wat</div>;
+}
